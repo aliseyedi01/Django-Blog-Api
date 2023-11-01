766e731a078ed9a72af5575f24af5a1be16ed3a1
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-11-01 15:04:10 +0330

    feat : add django rest framework

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd apps
+    'rest_framework',
+
+    # my apps
+    'restapi'
 ]
 
 MIDDLEWARE = [
