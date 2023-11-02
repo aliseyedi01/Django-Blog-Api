e14bc33ccff7755ee27399aaae3d09c34319644e
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-11-02 16:28:56 +0330

    feat : add model , view , controller , user , data

--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
